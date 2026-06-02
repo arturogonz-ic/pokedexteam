13a169156886bbdf0dc8d1b2a64b24de42b2247d
arturogonz-ic <arturo.gonzalez@iconos.mx>
2026-06-02 16:54:56 -0700

    End

--- src/app/page.js
+++ src/app/page.js
@@ -1,65 +1,69 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.js file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
+    <>
+    <header>
+      <button>
+        Regresar
+      </button>
+      <div>
+        <h2 id="title">
+          Crear Nuevo Equipo
+        </h2>
+        <div id="team">
+        <form>
+          <table border={1}>
+          <th><input type='text' placeholder="Nombre del Equipo" required></input></th>
+          <th><input type='text' placeholder="Creador del Equipo" required></input></th>
+          <th><input type='text' placeholder="Descripcion" required></input></th>
+          <th><button type="submit">Crear</button></th>
+        </table>
+        </form>
         </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
-    </div>
+      </div>
+    </header>
+    <main id="container">
+    <template id="pokemondiv">
+      <div id="pokemondiv">
+        <label id="idPokemon"></label>
+        <label id="namePokemon"></label>
+        <button id="anadirPokemon"></button>
+      </div>
+    </template> 
+    </main>
+    </>
   );
 }
+
+const frag = document.createDocumentFragment();
+const main = document.querySelector("main");
+const template = document.createElement("template");
+main.innerHTML = "";
+
+document.addEventListener("DOMContentLoaded", () => {
+    getPokemonApi();
+});
+
+const getPokemonApi = async () =>{
+  for(let id=1;id<=1;id++){
+    const respuesta = fetch(`https://pokeapi.co/api/v2/pokemon/${id}`);
+    const data = await respuesta.json;
+    console.log(data)
+    if (data){
+      paintPokemon(data);
+    }
+  }
+}
+const paintPokemon = (data) =>{
+  template.innerHTML(`
+    <img src=${data.sprites.front_default} alt="pokemonImg" class="pokemonSprite"></img>
+        <form method="POST">
+          <label class="pokemonId">
+            ${data.id}
+          </label>
+          <label class="pokemonName">
+            ${data.name}
+          </label>
+          <button type="submit" class="buttonAnadir">Anadir</button>
+        </form>
+    `)
+  }
